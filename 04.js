cc3f932fe4d844fbc1cd43306a7d7d056816a895
fajarhm22 <fajar.hidayahtullah22@gmail.com>
2025-10-02 12:21:17 +0700

    Update 04.js

--- 04.js
+++ 04.js
@@ -1,48 +1,44 @@
 class Mahasiswa {
     constructor(nama, nim, nilai) {
         this.nama = nama;   // Atribut publik
-        this.nim = nim;     // Atribut publik
-        let _nilai = nilai; // Atribut private menggunakan closure
+        this.nilai = nilai; // Atribut publik
+        let _nim = nim;     // Atribut private menggunakan closure
 
-        // Getter untuk mengakses nilai
-        this.getNilai = function() {
-            return _nilai;
+        // Getter untuk mengakses NIM
+        this.getNim = function() {
+            return _nim;
         };
 
-        // Setter untuk mengubah nilai dengan validasi
-        this.setNilai = function(nilaiBaru) {
-            if (nilaiBaru >= 0 && nilaiBaru <= 100) {
-                _nilai = nilaiBaru;
+        // Setter untuk mengubah NIM dengan validasi
+        this.setNim = function(nimBaru) {
+            if (typeof nimBaru === "string" && nimBaru.length >= 8) {
+                _nim = nimBaru;
             } else {
-                console.log('Nilai harus antara 0 dan 100');
+                console.log('NIM harus berupa string minimal 8 karakter');
             }
         };
 
         // Method untuk menampilkan informasi mahasiswa
         this.tampilkanInfo = function() {
             console.log(`Nama  : ${this.nama}`);
-            console.log(`NIM   : ${this.nim}`);
-            console.log(`Nilai : ${_nilai}`);
+            console.log(`NIM   : ${_nim}`);
+            console.log(`Nilai : ${this.nilai}`);
             console.log("-----------------------");
         };
     }
 }
 
 
-let prod1 = new Mahasiswa("Fajar Hidayahtullah", "202401110010", 85);
-let prod2 = new Mahasiswa("Shaula", "202401110040", 85);
+let prod1 = new Mahasiswa("Fajar Hidayahtullah", "202401110010", 80);
+let prod2 = new Mahasiswa("Shaula", "202401110040", 75);
 
 
 console.log("=== Data Mahasiswa ===");
 prod1.tampilkanInfo();
 prod2.tampilkanInfo();
 
+prod1.setNim("87654321");
 
-prod1.setNilai(90);
-prod2.setNilai(95);
-
-
-console.log("=== Setelah Nilai Diubah ===");
+console.log("=== Data Setelah Update ===");
 prod1.tampilkanInfo();
-prod2.tampilkanInfo();
 
